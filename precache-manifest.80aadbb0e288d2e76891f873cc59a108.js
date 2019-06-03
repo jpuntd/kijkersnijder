38115a684175d85a47deb796c4898a8834436abf
snwclone <jpuntd@gmail.com>
2019-06-03 09:24:00 +0200

    Updates

---
+++ precache-manifest.80aadbb0e288d2e76891f873cc59a108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3b716eb9dc03a8e54a2b28f4642f45",
+    "url": "/kijkersnijder/index.html"
+  },
+  {
+    "revision": "6437c821bae851a9fddf",
+    "url": "/kijkersnijder/static/css/2.1d5443d6.chunk.css"
+  },
+  {
+    "revision": "5eee8cb4ac30decb91fa",
+    "url": "/kijkersnijder/static/css/main.84390835.chunk.css"
+  },
+  {
+    "revision": "6437c821bae851a9fddf",
+    "url": "/kijkersnijder/static/js/2.69eb3b9b.chunk.js"
+  },
+  {
+    "revision": "5eee8cb4ac30decb91fa",
+    "url": "/kijkersnijder/static/js/main.3147ee99.chunk.js"
+  },
+  {
+    "revision": "c3754b8f1c0b9c1653c0",
+    "url": "/kijkersnijder/static/js/runtime~main.c133200a.js"
+  }
+]);
